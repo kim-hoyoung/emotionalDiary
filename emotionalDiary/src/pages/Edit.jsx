e84e40ecc85afcbb1b5bbad1d98ba7107e12c53a
kim-hoyoung <happy3sk@naver.com>
2025-02-17 23:57:09 +0900

    feat: Editor 기능, Diary 기능 구현

--- emotionalDiary/src/pages/Edit.jsx
+++ emotionalDiary/src/pages/Edit.jsx
@@ -1,14 +1,17 @@
-import { useParams, useNavigate } from "react-router-dom"; //URL parameter로 동적 경로 설정
+import { useParams, useNavigate, replace } from "react-router-dom"; //URL parameter로 동적 경로 설정
 import Header from "../components/Header";
 import Button from "../components/Buttons";
 import Editor from "../components/Editor";
-import { useContext } from "react";
-import { DiaryDispatchContext } from "../App";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const { onDelete } = useContext(DiaryDispatchContext);
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+
+  const curDiaryItem = useDiary(params.id);
 
   const onClickDelete = () => {
     if (
@@ -20,6 +23,18 @@ const Edit = () => {
     } // 일기 종료 로직
   };
 
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav("/", { replace: true });
+    }
+  };
+
   return (
     <div>
       <Header
@@ -29,7 +44,7 @@ const Edit = () => {
           <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
         }
       />
-      <Editor />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   );
 };
